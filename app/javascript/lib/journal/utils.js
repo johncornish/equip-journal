a7649431cf263274ecd41ead763551911a513716
John Cornish <johncornishthe4th@gmail.com>
2020-04-06 22:24:53 -0600

    >:(

--- app/javascript/lib/journal/utils.js
+++ app/javascript/lib/journal/utils.js
@@ -6,6 +6,7 @@ export const entriesToCollections = entries => entries
 
 const pageReducer = (acc, e) => {
   if (acc.length === 0) {
+    // https://www.ruby-forum.com/t/date-format-and-parsing-to-javascript-date/142276
     const d = Date.parse(e.created_at)
     return [
       {
