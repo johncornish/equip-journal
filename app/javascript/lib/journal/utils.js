bcbc42b2fdaf426ebcfceafe17cd85b7816c17a1
John Cornish <johncornishthe4th@gmail.com>
2020-04-11 22:55:51 -0600

    Group entries by day and collection

--- app/javascript/lib/journal/utils.js
+++ app/javascript/lib/journal/utils.js
@@ -4,27 +4,40 @@ export const entriesToCollections = entries => entries
   .filter(e => e.collection)
   .map(e => e.collection)
 
-const pageReducer = (acc, e) => {
-  if (acc.length === 0) {
-    // https://www.ruby-forum.com/t/date-format-and-parsing-to-javascript-date/142276
-    const d = Date.parse(e.created_at_js)
-    return [
-      {
-        title: moment(d).format('M-d-YYYY'),
-        entries: [e],
-      }
-    ]
+const titlesFromEntry = entry => {
+  const titles = []
+  const d = Date.parse(entry.created_at_js)
+  titles.push(moment(d).format('M-d-YYYY'))
+
+  if (entry.collection) {
+    titles.push(entry.collection)
   }
-  const lastEntry = acc.pop()
-  return [
-    ...acc,
-    {
-      title: lastEntry.title,
-      entries: [
-        ...lastEntry.entries,
-        e,
-      ],
-    },
-  ]
+
+  return titles
 }
-export const entriesToPages = entries => entries.reduce(pageReducer, [])
+
+const sortByCreationTime = (a, b) => {
+  const aDate = Date.parse(a.created_at_js)
+  const bDate = Date.parse(b.created_at_js)
+  return aDate - bDate
+}
+export const entriesToPages = entries => {
+  const sortedEntries = entries.sort(sortByCreationTime)
+  let pages = []
+  for (let e of sortedEntries) {
+    const pageTitles = titlesFromEntry(e)
+    for (let t of pageTitles) {
+      const pageIndex = pages.findIndex(page => page.title === t)
+      if (pageIndex === -1) {
+        pages.push({
+          title: t,
+          entries: [e],
+        })
+      } else {
+        pages[pageIndex].entries.push(e)
+      }
+    }
+  }
+
+  return pages
+}
--- app/javascript/lib/journal/utils.test.js
+++ app/javascript/lib/journal/utils.test.js
@@ -27,63 +27,52 @@ describe('entriesToCollections', () => {
 describe('entriesToPages', () => {
   it('should group entries by date and collection', () => {
     const entries = [
-      {text: "Test entry text 1", created_at_js: "2000-01-02 01:01:01.001"},
-      {text: "Test entry text 2", created_at_js: "2000-01-02 01:01:01.001"},
+      {text: "Test entry text 1", created_at_js: "2000-01-07 01:02:01.001", collection: 'Test collection 1'},
+      {text: "Test entry text 2", created_at_js: "2000-01-02 01:02:01.001"},
+      {text: "Test entry text 3", created_at_js: "2000-01-03 01:01:01.001"},
+      {text: "Test entry text 4", created_at_js: "2000-01-03 01:02:01.001", collection: 'Test collection 2'},
+      {text: "Test entry text 5", created_at_js: "2000-01-02 01:01:01.001", collection: 'Test collection 3'},
     ]
     const expectedPages = [
       {
         title: '1-2-2000',
         entries: [
-          {text: "Test entry text 1", created_at_js: "2000-01-02 01:01:01.001"},
-          {text: "Test entry text 2", created_at_js: "2000-01-02 01:01:01.001"},
+          {text: "Test entry text 5", created_at_js: "2000-01-02 01:01:01.001", collection: 'Test collection 3'},
+          {text: "Test entry text 2", created_at_js: "2000-01-02 01:02:01.001"},
         ]
-      }
+      },
+      {
+        title: 'Test collection 3',
+        entries: [
+          {text: "Test entry text 5", created_at_js: "2000-01-02 01:01:01.001", collection: 'Test collection 3'},
+        ]
+      },
+      {
+        title: '1-3-2000',
+        entries: [
+          {text: "Test entry text 3", created_at_js: "2000-01-03 01:01:01.001"},
+          {text: "Test entry text 4", created_at_js: "2000-01-03 01:02:01.001", collection: 'Test collection 2'},
+        ]
+      },
+      {
+        title: 'Test collection 2',
+        entries: [
+          {text: "Test entry text 4", created_at_js: "2000-01-03 01:02:01.001", collection: 'Test collection 2'},
+        ]
+      },
+      {
+        title: '1-7-2000',
+        entries: [
+          {text: "Test entry text 1", created_at_js: "2000-01-07 01:02:01.001", collection: 'Test collection 1'},
+        ]
+      },
+      {
+        title: 'Test collection 1',
+        entries: [
+          {text: "Test entry text 1", created_at_js: "2000-01-07 01:02:01.001", collection: 'Test collection 1'},
+        ]
+      },
     ]
     expect(entriesToPages(entries)).toStrictEqual(expectedPages)
   })
 })
-//
-// test "group journal entries by date and collection" do
-//   j = Journal.create!(
-//     name: 'test-journal',
-//   )
-//
-//   t = Time.local(2000, 1, 1, 10, 5, 0)
-//   Timecop.travel(t)
-//   je1 = JournalEntry.create!(
-//     journal: j,
-//     text: 'test-entry-text-1',
-//     collection: 'test-collection-text-1',
-//   )
-//   je2 = JournalEntry.create!(
-//     journal: j,
-//     text: 'test-entry-text-2',
-//   )
-//   t = Time.local(2000, 1, 1, 10, 0, 0)
-//   Timecop.travel(t)
-//   je3 = JournalEntry.create!(
-//     journal: j,
-//     text: 'test-entry-text-3',
-//     collection: 'test-collection-text-2',
-//   )
-//   t = Time.local(2000, 1, 1, 9, 0, 0)
-//   Timecop.travel(t)
-//   je4 = JournalEntry.create!(
-//     journal: j,
-//     text: 'test-entry-text-4',
-//   )
-//   t = Time.local(2000, 1, 12, 10, 0, 0)
-//   Timecop.travel(t)
-//   je5 = JournalEntry.create!(
-//     journal: j,
-//     text: 'test-entry-text-5',
-//   )
-//
-//   expected = ActiveSupport::OrderedHash.new
-//   expected['1-1-2000'] = [je4, je3, je1, je2]
-//   expected['test-collection-text-2'] = [je3]
-//   expected['test-collection-text-1'] = [je1]
-//   expected['1-12-2000'] = [je5]
-//
-//   assert_equal expected, j.journal_entries_by_collection
-// end
